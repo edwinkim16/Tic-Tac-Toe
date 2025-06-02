5169844893ffa11f910302ea40d7b57f2d171021
edwinkim16 <edwinkimathi226@gmail.com>
2025-06-02 17:49:17 +0300

    Using props to update values

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 
@@ -11,19 +11,19 @@ function App() {
   return (
     <div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </div>
   );
